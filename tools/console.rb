105d97158bb3764788fb7f964449ec319298b9ee
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-22 19:13:06 +0100

    new run file for Lyft with seeds

--- tools/console.rb
+++ tools/console.rb
@@ -4,23 +4,5 @@ def reload
   load 'config/environment.rb'
 end
 
-harry = Passenger.new("Harry")
-darren = Passenger.new("Darren")
-james = Passenger.new("James")
-nick = Passenger.new("Nick")
-elliot = Passenger.new("Elliott")
-
-d1 = Driver.new("driver1")
-d2 = Driver.new("driver2")
-d3 = Driver.new("driver3")
-d4 = Driver.new("driver4")
-d5 = Driver.new("driver5")
-
-r1 = Ride.new(24, d1, harry)
-r2 = Ride.new(3, d2, darren)
-r3 = Ride.new(26, d3, harry)
-r4 = Ride.new(7, d4, james)
-r5 = Ride.new(45, d5, harry)
-r6 = Ride.new(20, d3, harry)
 
 Pry.start
